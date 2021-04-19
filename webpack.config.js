d647ab0e70e330ea4b9500326c5b3c06478185bd
wandji <wbertrand10@gmail.com>
2021-04-19 09:16:58 +0100

    Added library package

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,12 @@
 const path = require('path');
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+  },
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
